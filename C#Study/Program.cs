66c26abb297a1a6ad75556fa303c24c31ef4cb3e
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-04 20:01:39 +0700

    add

--- C#Study/Program.cs
+++ C#Study/Program.cs
@@ -2,9 +2,13 @@
 {
     public static void Main(string[] args)
     {
+        const float PI = 3.14f;
         int a = 1;
         int b = 2;
         int c = a + b;
         Console.WriteLine(c);
+        string name = "Nam";
+        b += 2;
+        Console.WriteLine(b);
     }
 }
