a7f64f54a7febd4027b0b0b37b3f97fe70ac94f7
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-04 20:17:43 +0700

    add

---
+++ Accuracy/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+{
+    float StyleUSD = 25000;
+        int usd = 1000;
+        Console.WriteLine("" + usd*StyleUSD);
+}
+
+}
--- C#Study/Program.cs
+++ C#Study/Program.cs
@@ -2,13 +2,8 @@
 {
     public static void Main(string[] args)
     {
-        const float PI = 3.14f;
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        Console.WriteLine(c);
-        string name = "Nam";
-        b += 2;
-        Console.WriteLine(b);
+        int a = 9;
+        Console.WriteLine(a / 2);
+        Console.WriteLine(a % 2);
     }
 }
