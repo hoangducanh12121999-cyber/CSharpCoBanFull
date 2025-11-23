a68c3b95979d31b165d8695d840c95b15fe3932f
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-24 01:32:43 +0700

    add
    add

---
+++ VariableDataType/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int i = 5;
+        float f = 1.5f;
+        double d = 4.2;
+        Boolean b = true;
+        char c = 'a';
+        String s = "Hello CodeGym";
+        Console.WriteLine($"Giá trị của biến i là {i}");
+        Console.WriteLine($"Giá trị của biến f là {f}");
+        Console.WriteLine($"Giá trị của biến d là {d}");
+        Console.WriteLine($"Giá trị của biến b là {b}");
+        Console.WriteLine($"Giá trị của biến c là {c}");
+        Console.WriteLine($"Giá trị của biến s là {s}");
+    }
+}
