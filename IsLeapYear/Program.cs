2b781578ba2eb783f34369d865e1bfda429e88db
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-24 02:15:55 +0700

    add

---
+++ IsLeapYear/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hay nhap mot nam bat ky:");
+        int year = Convert.ToInt32(Console.ReadLine());
+        bool isLeapYear = true;
+
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0) isLeapYear = true;
+                else isLeapYear = false;
+            }
+            else isLeapYear = true;
+        }
+        else isLeapYear = false;
+
+        if (isLeapYear)
+        {
+            Console.WriteLine($"{year} la nam nhuan");
+        }
+        else
+        {
+            Console.WriteLine($"{year} khong phai nam nhuan");
+        }
+    }
+}
