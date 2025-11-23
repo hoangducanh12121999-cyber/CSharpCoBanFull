17e93dfcb738b27d3f9db5998b849241f237c183
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-24 01:56:33 +0700

    add

---
+++ MoneyConvesion/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hay nhap gia tien USD:");
+        double moneyUDS = Convert.ToDouble(Console.ReadLine());
+        double moneyVND = moneyUDS * 23000;
+        Console.WriteLine($"Gia tri tien te theo VND la: {moneyVND} VND");
+    }
+}
