d2f4f854abfd26805e0d9e8a7173ab5f8fca421f
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-24 01:48:56 +0700

    add
    add

---
+++ HienThiLoiChao/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string yourName = Convert.ToString(Console.ReadLine());
+        Console.WriteLine($"Hello {yourName}");
+    }
+}
