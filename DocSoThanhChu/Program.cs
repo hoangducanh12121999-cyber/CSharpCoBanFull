a2784e47ea144fad3ffda0980335a4694a9ad75b
hoangducanh12121999-cyber <hoangducanh12121999@gmail.com>
2025-11-24 21:56:58 +0700

    add

--- DocSoThanhChu/Program.cs
+++ DocSoThanhChu/Program.cs
@@ -4,118 +4,141 @@
     {
         Console.WriteLine("Hay nhap mot so co toi da 3 chu so");
         int number = Convert.ToInt32(Console.ReadLine());
-        string donvi;
-        string chuc;
-        string tram;
-        if (number >= 0 && number < 100)
-        {
-            int i = number / 10;
-            int j = number % 10;
-            if (i == 0)
-            {
-                switch (j)
-                {
-                    case 0:
-                        donvi = "Khong";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 1:
-                        donvi = "Mot";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 2:
-                        donvi = "Hai";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 3:
-                        donvi = "Ba";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 4:
-                        donvi = "Bon";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 5:
-                        donvi = "Nam";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 6:
-                        donvi = "Sau";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 7:
-                        donvi = "Bay";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 8:
-                        donvi = "Tam";
-                        Console.WriteLine(donvi);
-                        break;
-                    case 9:
-                        donvi = "Chin";
-                        Console.WriteLine(donvi);
-                        break;
-                }
-            }
-            else if (1 <= i && i <= 9)
-            {
-                if (j == 0)
-                {
-                    switch (i)
-                    {
-                        case 1:
-                        chuc = "Muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 2:
-                        chuc = "Hai muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 3:
-                        chuc = "Ba muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 4:
-                        chuc = "Bon muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 5:
-                        chuc = "Nam muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 6:
-                        chuc = "Sau muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 7:
-                        chuc = "Bay muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 8:
-                        chuc = "Tam muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        case 9:
-                        chuc = "Chin muoi";
-                        Console.WriteLine(chuc);
-                        break;
-                        default:
-                        break;
-                    }
-                }
+        int hangdonvi = number % 10;
+        int hangchuc = (number / 10) % 10;
+        int hangtram = number / 100;
+        string tram = "";
+        string chuc = "";
+        string donvi ="";
+        string read = "";
 
+        switch (hangtram)
+        {
+            case 1:
+                tram = "một trăm";
+                break;
+            case 2:
+                tram = "hai trăm";
+                break;
+            case 3:
+                tram = "ba trăm";
+                break;
+            case 4:
+                tram = "bốn trăm";
+                break;
+            case 5:
+                tram = "năm trăm";
+                break;
+            case 6:
+                tram = "sáu trăm";
+                break;
+            case 7:
+                tram = "bảy trăm";
+                break;
+            case 8:
+                tram = "tám trăm";
+                break;
+            case 9:
+                tram = "chín trăm";
+                break;
+        }
+
+        if (hangtram != 0 && hangchuc == 0 && hangdonvi != 0)
+        {
+            chuc = "linh";
+        }
+        else
+        {
+            switch (hangchuc)
+            {
+                case 1:
+                    chuc = "mười";
+                    break;
+                case 2:
+                    chuc = "hai mươi";
+                    break;
+                case 3:
+                    chuc = "ba mươi";
+                    break;
+                case 4:
+                    chuc = "bốn mươi";
+                    break;
+                case 5:
+                    chuc = "năm mươi";
+                    break;
+                case 6:
+                    chuc = "sáu mươi";
+                    break;
+                case 7:
+                    chuc = "bảy mươi";
+                    break;
+                case 8:
+                    chuc = "tám mươi";
+                    break;
+                case 9:
+                    chuc = "chín mươi";
+                    break;
+            }
+        }
+
+        switch (hangdonvi)
+        {
+            case 1:
+                if (hangchuc > 1)
+                {
+                    donvi = "mốt";
+                }
                 else
                 {
-                    switch (j)
-                    {
-                        case 1:
-                        Console.WriteLine($"{chuc} {donvi}");
-                        break;
-                    }
+                    donvi = "một";
                 }
-            }
-
+                break;
+            case 2:
+                donvi = "hai";
+                break;
+            case 3:
+                donvi = "ba";
+                break;
+            case 4:
+                donvi = "bốn";
+                break;
+            case 5:
+                if (hangchuc > 0) 
+                {
+                    donvi = "lăm";
+                }
+                else 
+                {
+                    donvi = "năm";
+                }
+                break;
+            case 6:
+                donvi = "sáu";
+                break;
+            case 7:
+                donvi = "bảy";
+                break;
+            case 8:
+                donvi = "tám";
+                break;
+            case 9:
+                donvi = "chín";
+                break;
         }
-        
+
+        if (number == 0) read = "Không";
+        if (hangtram != 0)
+        {
+            read = tram + " " + chuc + " " + donvi;
+        }
+        else if (hangtram == 0)
+        {
+            if (hangchuc > 0)
+            read = chuc + " " + donvi;
+            else 
+            read = donvi;
+        }
+        Console.WriteLine("Cach doc: " + read);
+
     }
 }
